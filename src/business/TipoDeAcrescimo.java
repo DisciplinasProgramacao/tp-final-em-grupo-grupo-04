9f2ef3dac80d30277f2e6ee1854128ab1e4774cf
pedro-henrique107 <72760426+pedro-henrique107@users.noreply.github.com>
2022-06-17 01:12:08 -0300

    Base para Enum Tipo de Acréscimo

---
+++ src/business/TipoDeAcrescimo.java
@@ -0,0 +1,11 @@
+package Business;
+
+public enum TipoDeAcrescimo {
+pepperoni(4),bacon(3),palmito(3),queijo(2),picles(2),ovo(2),batata_palha(2);
+	double preco;
+	TipoDeAcrescimo(double preco) {
+		this.preco = preco;
+		
+	}
+
+}
